36b399ca24da5acbe1ae9582834c151da2dd938c
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2016-02-11 12:40:56 +0100

    bootstrap

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
 
   # You can have the root of your site routed with "root"
   root 'static#dashboard'
+  get "stats" => "static#stats"
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
