67dde45f6319aab64d3d21f884b615948a5ab12f
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2016-02-16 21:53:52 +0100

    rubocop fix

--- app/models/player.rb
+++ app/models/player.rb
@@ -8,12 +8,12 @@ class Player < ActiveRecord::Base
     presences.joins(:match).where("matches.played = 't'").each do |presence|
       if (presence.team == 'black' && presence.match.black_goals > presence.match.white_goals) ||
          (presence.team == 'white' && presence.match.black_goals < presence.match.white_goals)
-        won+ = 1
+        won += 1
       elsif (presence.team == 'black' && presence.match.black_goals < presence.match.white_goals) ||
             (presence.team == 'white' && presence.match.black_goals > presence.match.white_goals)
-        lost+ = 1
+        lost += 1
       elsif presence.match.white_goals == presence.match.black_goals
-        tie+ = 1
+        tie += 1
       end
     end
     [presences.joins(:match).where("matches.played = 't'").count, won, lost, tie]
