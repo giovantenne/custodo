2ec0b53dba73f38aaa751f17a4c1dde87911f678
Claudio Benvenuti <claudio.benvenuti@gmail.com>
2016-06-09 09:51:15 +0200

    cached stats pairs in helper

--- app/controllers/static_controller.rb
+++ app/controllers/static_controller.rb
@@ -20,32 +20,4 @@ class StaticController < ApplicationController
     end
   end
 
-  def stats
-    @good_pairs = []
-    @good_score = 0
-    Player.order(:name).each do |p1|
-      Player.order(:name).each do |p2|
-        v = Presence.joins(:match).where("(player_id = ? or player_id = ?) and ((team = 'white' AND matches.white_goals > matches.black_goals  ) OR (team = 'black' AND matches.black_goals > matches.white_goals))", p1, p2).having("COUNT(\"presences\".\"id\") = 2").group(:match_id,:team).count(:id).count
-        if v > @good_score 
-          @good_score = v
-          @good_pairs = [[p1,p2]]
-        elsif v == @good_score and !@good_pairs.include?([p2,p1])
-          @good_pairs << [p1,p2]
-        end
-      end
-    end
-    @bad_pairs = []
-    @bad_score = 0
-    Player.order(:name).each do |p1|
-      Player.order(:name).each do |p2|
-        v = Presence.joins(:match).where("(player_id = ? or player_id = ?) and ((team = 'white' AND matches.white_goals < matches.black_goals  ) OR (team = 'black' AND matches.black_goals < matches.white_goals))", p1, p2).having("COUNT(\"presences\".\"id\") = 2").group(:match_id,:team).count(:id).count
-        if v > @bad_score 
-          @bad_score = v
-          @bad_pairs = [[p1,p2]]
-        elsif v == @bad_score and !@bad_pairs.include?([p2,p1])
-          @bad_pairs << [p1,p2]
-        end
-      end
-    end
-  end
 end
